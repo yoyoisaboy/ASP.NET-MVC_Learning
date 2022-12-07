5f826e10dc6c60e456364342930867395c526044
yoyoisaboy <kroy1002@example.com>
2022-12-07 09:21:30 +0800

    upload Day13

---
+++ Day13/Day13/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day13.Models;
+namespace Day13.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string keyword)
+        {
+            var custs = db.客戶.Where(m => m.地址.Contains(keyword)).
+                ToList();
+            return View(custs);
+        }
+    }
+}
---
+++ Day13/Day13/Controllers/SearchAddressController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Day13.Models;
+namespace Day13.Controllers
+{
+    public class SearchAddressController : ApiController
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: api/SearchAddress
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SearchAddress/5
+        public List<string> Get(string keyword)
+        {
+            var custs = db.客戶.Where(m => m.地址.Contains(keyword)).ToList();
+            List<string> address = new List<string>();
+            foreach (var c in custs)
+            {
+                address.Add(c.地址);
+            }
+            return address.Distinct().ToList();
+        }
+
+        // POST: api/SearchAddress
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/SearchAddress/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/SearchAddress/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
